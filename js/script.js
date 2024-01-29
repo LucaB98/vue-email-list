6298af81ea455ad34768851a218a1575c799ca45
LucaB98 <lucabellan98@gmail.com>
2024-01-29 14:38:34 +0100

    create computed for emails

--- js/script.js
+++ js/script.js
@@ -1,8 +1,16 @@
 const {createApp} = Vue;
+const endpoint = 'https://flynn.boolean.careers/exercises/api/random/mail'
 const app = createApp({
-    data(){
-        return{
-            message: 'Hello Vue!',
+    data: () => ({
+        emails:[]
+    }),
+    computed:{
+        getEmails(){
+            for(let i = 0; i < 10; i++){
+                    axios.get(endpoint).then(res => {
+                    this.emails.push(res.data.response)
+                })
+                }
         }
     }
 })
